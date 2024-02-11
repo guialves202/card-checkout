1bfdb70d7c2281236cfcb1a394ffe9bf2f7bba0c
guialves202 <guimacielalves2@gmail.com>
2024-02-11 11:49:05 -0300

    :tada: First Commit

---
+++ api/prisma/migrations/20240211144122_init/migration.sql
@@ -0,0 +1,54 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "ownerFullName" TEXT NOT NULL,
+    "ownerCPF" TEXT NOT NULL,
+    "branch" TEXT NOT NULL,
+    "accountNumber" TEXT NOT NULL,
+    "balance" REAL NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "debitCardId" TEXT,
+    "creditCardId" TEXT,
+    CONSTRAINT "Account_debitCardId_fkey" FOREIGN KEY ("debitCardId") REFERENCES "DebitCard" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Account_creditCardId_fkey" FOREIGN KEY ("creditCardId") REFERENCES "CreditCard" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "DebitCard" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "number" TEXT NOT NULL,
+    "ownerNameOnCard" TEXT NOT NULL,
+    "securityCode" TEXT NOT NULL,
+    "accountId" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "CreditCard" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "number" TEXT NOT NULL,
+    "ownerNameOnCard" TEXT NOT NULL,
+    "securityCode" TEXT NOT NULL,
+    "limit" REAL NOT NULL,
+    "currentInvoice" REAL NOT NULL,
+    "accountId" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Statement" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "statementDate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "TransactionValue" REAL NOT NULL,
+    "accountId" TEXT,
+    CONSTRAINT "Statement_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DebitCard_number_key" ON "DebitCard"("number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CreditCard_number_key" ON "CreditCard"("number");
