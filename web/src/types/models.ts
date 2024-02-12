252862b82488379e2660a895909c9315f7857b7d
guialves202 <guimacielalves2@gmail.com>
2024-02-12 20:44:13 -0300

    :sparkles: feat: main page image, axios lib and model types

---
+++ web/src/types/models.ts
@@ -0,0 +1,29 @@
+export type Account = {
+  ownerFullName: string
+  ownerCPF: string
+  branch: string
+  accountNumber: string
+  balance: number
+}
+
+export type DebitCard = {
+  number: string
+  ownerNameOnCard: string
+  securityCode: string
+  accountId?: string
+}
+
+export type CreditCard = {
+  number: string
+  ownerNameOnCard: string
+  securityCode: string
+  accountId?: string
+  limit: number
+  currentInvoice: number
+}
+
+export type Statement = {
+  statementDate: Date
+  TransactionValue: number
+  accountId: string
+}
